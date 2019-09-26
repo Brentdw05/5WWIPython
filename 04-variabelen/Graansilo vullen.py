be88afbbab12c99b346c363ff9fcc14ed6741668
Brentdw05 <brent.dewinter@sgsintpaulus.eu>
2019-09-26 16:00:43 +0200

    graansilo vullen

---
+++ 04-variabelen/Graansilo vullen.py
@@ -0,0 +1,16 @@
+#invoer
+b= input('breedte van het veld: ')
+l=input( 'lengte van het veld: ')
+c= input( 'aantal kubieke meter graan per hectare: ')
+h= input( 'hoogte van de silo: ')
+r= input( 'straal van de silo: ')
+
+#berekening
+pi= 3.14159265359
+volume_silo= pi*r*r*h
+aantal_silo= volume_silo//c
+hoogte_laatste_silo=  volume_silo%c
+
+#uitvoer
+print(aantal_silo)
+print(hoogte_laatste_silo)
