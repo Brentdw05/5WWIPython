bc53c35c4425b42f6dca646c23ae085749dc745e
Brentdw05 <brent.dewinter@sgsintpaulus.eu>
2019-09-26 19:47:34 +0200

    graansilo vullen

--- 04-variabelen/Graansilo vullen.py
+++ 04-variabelen/Graansilo vullen.py
@@ -7,10 +7,12 @@ r= input( 'straal van de silo: ')
 
 #berekening
 pi= 3.14159265359
+aantal_hectare= (float(b)*float(l))/10000
 volume_silo= float(pi)*float(r)*float(r)*float(h)
-aantal_silo= volume_silo//float(c)
-hoogte_laatste_silo=  volume_silo%float(c)
+totale_opbrengst= float(c)*float(aantal_hectare)
+aantal_silo= volume_silo//totale_opbrengst
+hoogte_laatste_silo=  volume_silo%totale_opbrengst
 
 #uitvoer
-print(aantal_silo)
+print(aantal_silo+1)
 print(hoogte_laatste_silo)
