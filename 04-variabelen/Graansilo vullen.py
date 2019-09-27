a7f82fec7cdf0c449065975dab657ff02d73490c
Brentdw05 <brent.dewinter@sgsintpaulus.eu>
2019-09-27 11:48:33 +0200

    tijdmeting op mars

--- 04-variabelen/Graansilo vullen.py
+++ 04-variabelen/Graansilo vullen.py
@@ -1,18 +1,20 @@
 #invoer
-b= input('breedte van het veld: ')
-l=input( 'lengte van het veld: ')
-c= input( 'aantal kubieke meter graan per hectare: ')
-h= input( 'hoogte van de silo: ')
-r= input( 'straal van de silo: ')
+b= float(input('breedte van het veld: '))
+l= float(input( 'lengte van het veld: '))
+c= float(input( 'aantal kubieke meter graan per hectare: '))
+h= float(input( 'hoogte van de silo: '))
+r= float(input( 'straal van de silo: '))
 
 #berekening
 pi= 3.14159265359
-aantal_hectare= (float(b)*float(l))/10000
-volume_silo= float(pi)*float(r)*float(r)*float(h)
-totale_opbrengst= float(c)*float(aantal_hectare)
+aantal_hectare= (b*l)/10000
+volume_silo= pi*r**2*h
+totale_opbrengst= c*float(aantal_hectare)
 aantal_silo= volume_silo//totale_opbrengst
-hoogte_laatste_silo=  volume_silo%totale_opbrengst
+volume_silo= volume_silo%totale_opbrengst
+hoogte_laatste_silo= volume_silo/r**2
+aantal_silo += 1
 
 #uitvoer
-print(aantal_silo+1)
+print(int(aantal_silo))
 print(hoogte_laatste_silo)
