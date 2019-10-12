55053f5bfbbc551de280146f4e06664b47d8b565
Brentdw05 <brent.dewinter@sgsintpaulus.eu>
2019-10-12 14:23:56 +0200

    absolute waarde

--- 05-eenvoudige_functies/Absolute waarde.py
+++ 05-eenvoudige_functies/Absolute waarde.py
@@ -4,13 +4,13 @@
 #'\N{LESS-THAN OR EQUAL TO}'
 
  #invoer
-cijfer_1= input('cijfer 1= ')
-cijfer_2= input('cijfer 2= ')
+cijfer_1= float(input('cijfer 1= '))
+cijfer_2= float(input('cijfer 2= '))
  #berekening
-linkerlid = abs(cijfer_1)-abs(cijfer_2)
-rechterlid = cijfer_1 - cijfer_2
-linkerlid_1 = round(linkerlid, 4)
-rechterlid_1 = round(rechterlid, 4)
+linkerlid = abs(cijfer_1-cijfer_2)
+rechterlid = abs(abs(cijfer_1) - abs(cijfer_2))
+linkerlid = round(linkerlid, 4)
+rechterlid = round(rechterlid, 4)
 
  #uitvoer
-print(str(linkerlid_1) , '\N{LESS-THAN OR EQUAL TO}', str(rechterlid_1))
+print(str(rechterlid) , '\N{LESS-THAN OR EQUAL TO}', str(linkerlid))
