3224402bf66cd8ba4da4e5f06d13c7cefe88f8a9
Brentdw05 <brent.dewinter@sgsintpaulus.eu>
2019-10-12 14:06:13 +0200

    vis viva

---
+++ 05-eenvoudige_functies/vis viva.py
@@ -0,0 +1,22 @@
+from math import pi
+
+#invoer
+
+
+r= input('de afstand tussen de satelliet en het middelpunt van de aarde: ')
+
+v= input('de snelheid van de satelliet ten opzichte van de aarde: ')
+
+#berekening
+
+a= ( pow(r*10, -9) ) / 2 * pow(10, -9) - r*v**2
+
+p= 2 * pi * pow(pow(a, 3)/pow(10, -9), 0.5)
+
+print(a)
+print(p)
+
+
+
+
+#uitvoer
