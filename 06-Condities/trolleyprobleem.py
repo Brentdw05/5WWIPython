dba0fde333cfbd677db5fe4d92d33d11c4917ae5
Brentdw05 <brent.dewinter@sgsintpaulus.eu>
2019-10-18 11:56:45 +0200

    trolleyprobleem

---
+++ 06-Condities/trolleyprobleem.py
@@ -0,0 +1,17 @@
+#invoer
+
+antw_1= str(input('Trek aan de hendel van de wissel: '))
+antw_2= str(input('Man van de brug duwen: '))
+
+#berekening
+if antw_1 == 'ja' and antw_2 == 'ja':
+    doden= 2
+elif antw_1 == 'nee' and antw_2 == 'ja':
+    doden = 1
+elif antw_1 == 'ja' and antw_2 == 'nee':
+    doden = 1
+else:
+    doden = 5
+#uitvoer
+
+print(doden)
