1e6f5a4aeb4b6ba02ee99b06bb4b4be117920721
Brentdw05 <brent.dewinter@sgsintpaulus.eu>
2019-10-19 11:18:47 +0200

    schaal van Saffir-Simpson

--- 06-Condities/schaal van Saffir-Simpson.py
+++ 06-Condities/schaal van Saffir-Simpson.py
@@ -1,9 +1,22 @@
 #invoer
 
-windsnelheid = int(input())
+windsnelheid = int(input(' geef een windsnelheid: '))
 
 #berekening
 
-
+if windsnelheid >= 250:
+    uitvoer = 'categorie 5'
+elif windsnelheid <= 249 and windsnelheid >= 210:
+    uitvoer = 'categorie 4'
+elif windsnelheid <= 209 and windsnelheid >= 178:
+    uitvoer = 'categorie 3'
+elif windsnelheid <= 177 and windsnelheid >= 154:
+    uitvoer = 'categorie 2'
+elif windsnelheid <= 153 and windsnelheid >= 119:
+    uitvoer = 'categorie 1'
+else:
+    uitvoer = 'geen orkaan'
 
 #uitvoer
+
+print(uitvoer)
