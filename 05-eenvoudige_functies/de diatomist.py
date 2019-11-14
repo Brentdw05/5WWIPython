57d8edae5f5bab19626ea9117660661fd1359c11
Brentdw05 <brent.dewinter@sgsintpaulus.eu>
2019-11-14 19:30:40 +0100

    de diatomist

--- 05-eenvoudige_functies/de diatomist.py
+++ 05-eenvoudige_functies/de diatomist.py
@@ -7,9 +7,11 @@ grote_r = float(input('wat is de straal van de grote cirkel: '))
 #berekening
 
 n = 0.83*(grote_r**2/kleine_r**2) - 1.9
+n = int(n)
 
-procent_bedekt  = grote_r/kleine_r*100
+procent_bedekt  = grote_r/n*kleine_r*100
+procent_bedekt = round(procent_bedekt, 2)
 
 #uitvoer
 
-print(str(n), ' kleine cirkels bedekken ', str(procent_bedekt), 'van de grote cirkel' )
+print(str(n), 'kleine cirkels bedekken', str(procent_bedekt), '%van de grote cirkel' )
