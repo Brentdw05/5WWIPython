806c3f145b53bc8e065c056b3f184c9d3c7bd947
Brentdw05 <brent.dewinter@sgsintpaulus.eu>
2019-11-14 17:55:23 +0100

    gevoelstemperatuur

--- 05-eenvoudige_functies/gevoelstemperatuur.py
+++ 05-eenvoudige_functies/gevoelstemperatuur.py
@@ -1,12 +1,13 @@
 #invoer
 
-t= input('de luchtemperatuur in °C = ')
+t= float(input('de luchtemperatuur in °C = '))
 
-w= input('de windsnelheid in km/u = ')
+w= float(input('de windsnelheid in km/u = '))
 
 #berekening
 
-gevoelstemperatuur= 13.12 + 0.6215*int(t) + int(0.3965*int(t) - 11.37)*int(pow(3.6*int(w), 0.16))
+w = w/3.6
+gevoelstemperatuur= 13.12 + 0.6215*t + (0.3965*(t) - 11.37)*(pow(3.6*w, 0.16))
 
 #uitvoer
 
