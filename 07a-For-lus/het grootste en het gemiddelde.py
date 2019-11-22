e68f76bda5a0a3411886eba2a7f9d5e82dbd4eda
Brentdw05 <brent.dewinter@sgsintpaulus.eu>
2019-11-22 11:16:57 +0100

    het grootste en het gemiddelde

---
+++ 07a-For-lus/het grootste en het gemiddelde.py
@@ -0,0 +1,22 @@
+#invoer
+
+aantal = int(input('aantal getallen: '))
+
+getal_0 = int(input('geef getal : '))
+
+som, grootste = getal_0, getal_0
+
+#berekening
+
+for i in range(aantal - 1):
+    getal = int(input('geef getal '+ str(i + 1) + ': '))
+    som += getal
+    grootste = max(grootste, getal)
+
+gemiddelde = som/aantal
+
+uitvoer = 'Het grootste getal is {:.0f} en het gemiddelde is {:.2f}' .format(grootste, gemiddelde)
+
+#uitvoer
+
+print(uitvoer)
