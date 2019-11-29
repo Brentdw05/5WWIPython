6985335b1a7e5f9e186097173651a15a8d253794
Brentdw05 <brent.dewinter@sgsintpaulus.eu>
2019-11-29 11:20:05 +0100

    de rij van fibonacci

---
+++ 07a-For-lus/de rij van fibonacci.py
@@ -0,0 +1,15 @@
+
+getal = int(input('geef getal: '))
+
+getal_1 = 1
+getal_2 = 0
+
+for i in range(getal):
+    som = getal_1 + getal_2
+    getal_1 = som - getal_1
+    getal_2 = som
+
+
+print(som)
+
+
---
+++ 07b-while-lus/kassa.py
@@ -0,0 +1,11 @@
+
+som = 0
+prijs = 1
+
+prijs = int(input('geef de prijs:'))
+    if prijs == 0:
+        som += 0
+    else:
+        som += prijs
+print(som)
+
