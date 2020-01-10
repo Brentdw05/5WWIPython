be2154c8baf18986ea430183b85632859586b62d
Brentdw05 <brent.dewinter@sgsintpaulus.eu>
2020-01-10 11:41:39 +0100

    vouwen

--- 07b-while-lus/vouwen.py
+++ 07b-while-lus/vouwen.py
@@ -2,18 +2,18 @@
 
 papierdikte = int(input('geef de dikte van het papier in mm: '))
 afstand = int(input('geef de afstand van de aarde tot het hemellichaam in mm: '))
-vouwen = 1
-papierdikte_eind = 0
+vouwen = 0
+papierdikte_eind = 1
 
 #berekening
 
-while afstand != papierdikte:
+while afstand != papierdikte and afstand > papierdikte_eind:
     vouwen += 1
-    papierdikte_eind += papierdikte * 2
+    papierdikte_eind = papierdikte_eind * 2
 
 
 #uitvoer
 
-uitvoer = 'na {:.f} keer vouwen bedraagt de dikte van het papier {:.f} mm.'
+uitvoer = 'na {} keer vouwen bedraagt de dikte van het papier {} mm.'
 
 print(uitvoer.format(vouwen, papierdikte_eind))
