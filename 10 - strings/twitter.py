e89380f06723d0cc625525ad2a588f0e2078dacf
Brentdw05 <brent.dewinter@sgsintpaulus.eu>
2020-01-31 11:27:55 +0100

    twitter

--- 10 - strings/twitter.py
+++ 10 - strings/twitter.py
@@ -1,5 +1,14 @@
+bericht = input('geef een bericht: ')
+hashtag = bericht.find('#')
+woorden = ''
 
-tekst = input('geef een tweet: ')
+while hashtag != -1:
+    spatie = bericht[hashtag:].find(' ')
+    woorden += bericht[hashtag + 1 : hashtag + spatie] + '\n'
+
+    bericht = bericht[hashtag + spatie + 1:]
+    hashtag = bericht.find('#')
+
+print(woorden[:len(woorden) - 1])
 
 
-for i in range(0, len(tekst)):
