445e63645ba6981bf3f936e963401140c90a1f99
Brentdw05 <brent.dewinter@sgsintpaulus.eu>
2020-02-14 11:29:35 +0100

    Vlaggen

---
+++ 11 - Tuples/Vlaggen.py
@@ -0,0 +1,13 @@
+def vlag(richting, kleur):
+    uitvoer = ''
+    if richting == 'verticaal':
+        for i in kleur:
+                uitvoer += i
+                uitvoer += i + '|'
+    else:
+        pass
+
+    return uitvoer
+
+
+print(vlag('verticaal',('zwart', 'geel', 'rood')))
