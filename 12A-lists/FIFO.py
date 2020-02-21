4e191b62b9237cc7bbc1f5ea2a555734e9c28448
Brentdw05 <brent.dewinter@sgsintpaulus.eu>
2020-02-21 11:12:09 +0100

    FIFO

---
+++ 12A-lists/FIFO.py
@@ -0,0 +1,11 @@
+
+invoer = input()
+list = []
+
+while invoer != 'STOP':
+    if invoer != '?':
+        list.append(invoer)
+    elif len(list) > 0:
+        print(list.pop(0))
+    invoer = input()
+
